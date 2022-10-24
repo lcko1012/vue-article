82c45e8681010cafb70a867a8a278f4868caa841
Le Canh Kieu Oanh <le.canh.kieu.oanh@sun-asterisk.com>
2022-10-24 09:42:04 +0700

    set up environment

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <header class="flex p-3 border border-gray-100">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />
+  </header>
+
+  <RouterView />
+</template>
