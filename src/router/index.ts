58724a9e6702d65758a39069a058e9c70355a606
Le Canh Kieu Oanh <le.canh.kieu.oanh@sun-asterisk.com>
2022-10-24 16:10:23 +0700

    P2_show article details

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,21 @@
 import ArticlesView from "../views/ArticlesView.vue";
 import { createRouter, createWebHistory } from "vue-router";
+import ArticleDetailsView from "@/views/ArticleDetailsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{
-    path: "/",
-    name: "home",
-    component: ArticlesView
-  }],
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: ArticlesView
+    },
+    {
+      path: "/articles/:id",
+      name: "article_details",
+      component: ArticleDetailsView
+    }
+  ],
 });
 
 export default router;
--- src/services/ArticleDataService.ts
+++ src/services/ArticleDataService.ts
@@ -5,6 +5,9 @@ class ArticleDataService {
   async getAll(): Promise<IArticle[]>  {
     return await http.get("/articles");
   }
+  async getDetails(id: number): Promise<IArticle> {
+    return await http.get(`/articles/${id}`);
+  }
 }
 
 export default new ArticleDataService()
